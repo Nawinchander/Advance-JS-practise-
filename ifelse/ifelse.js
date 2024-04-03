1e4d4971ce502bf82ecca5c6c6af623d186b192d
Nawin Chander S <Nawinnapzy289@gmail.com>
2024-04-03 19:13:12 +0530

    Add files via upload

---
+++ ifelse/ifelse.js
@@ -0,0 +1,37 @@
+// if else condition statement
+
+// let Pwd_correct = true
+
+// if(Pwd_correct)
+//     console.log("you have logged in")
+// else
+//     console.log("incorrect password")
+
+// console.log("bye")
+
+let Pwd_correct = false
+
+if(Pwd_correct)
+    console.log("you have logged in")
+else
+    console.log("incorrect password")
+
+console.log("bye")
+
+let age = 62
+if(age<60)
+    console.log('you can proceed')
+else
+    console.log("you are not eligible")
+
+console.log("thank you for working with us")
+
+
+
+
+
+
+
+
+
+
