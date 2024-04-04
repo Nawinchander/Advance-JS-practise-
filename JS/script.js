9fb2b90f2e57e307e250c9ec9c73b3bb126c12af
Nawin Chander S <Nawinnapzy289@gmail.com>
2024-04-04 20:43:23 +0530

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,288 @@
+// let price = 100;
+// const product = 'TV';
+// let tax = 10;
+// console.log(product)
+// let Total = price + tax
+// console.log(Total)
+
+// var fruitname = 'apple'
+// var count = 10
+// var price = 100
+// let total = count * price
+// console.log(fruitname)
+// console.log('total price of the fruit ' + total)
+
+
+// var a = 10
+// a++
+// console.log(a)
+
+// var b = 10
+// ++b
+// console.log(b)
+
+// var c = 10
+// --c
+// console.log(c)
+
+// var d = 11
+// d--
+// console.log(d)
+
+// let x = 5
+// let y = ++x
+// console.log(x)
+// console.log(y)
+
+// console.log(typeof(10))
+// console.log(typeof('abcde'))
+// console.log(typeof(0.124))
+
+// function abc(){
+//     console.log('smart Nawin')
+// }
+// abc()
+
+
+// function phone(){
+//     console.log('this is an Iphone')
+//     console.log('this is an Oppo Phone')
+//     console.log('this is an redmi phone')
+//     }
+
+// phone()
+
+// function iphone(){
+//     console.log('this is an Iphone')
+// }
+
+// function oppo(){
+//     console.log('this is an Oppo Phone')
+// }
+
+// function redmi(){
+//     console.log('this is an redmi Phone')
+// }
+
+// iphone()
+// oppo()
+
+// redmi()
+
+// var a = 10
+// var b = 20
+
+// function add (){
+// console.log('the sum of the value = ', a+b)
+// }
+// add()
+
+// var factor = 'kamal'
+// var fplayer = 'Dhoni'
+// var fmovie = 'Anbe Sivam'
+
+// function fav(){
+//     console.log('favorite of actor = '+ factor)
+//     console.log('favorite of player = '+ fplayer)
+//     console.log('favorite of movie = '+ fmovie)
+// }
+// fav()
+
+// function add(a,b){
+//     console.log(a+b)
+// }
+
+// add(10,20)
+
+// function area(length,breath){
+//     console.log("area is "+ length*breath)
+// }
+
+// area(100,50)
+
+// function name(){
+//     return('nawin')
+// }
+
+// var a=name()
+// console.log(a)
+
+
+// function add(a,b){
+//     return(a+b)
+// }
+
+// var cm=add(2,3)
+// console.log(cm)
+
+// if(false)
+// {
+//     console.log('print true')
+// }
+// else
+// {
+//     console.log('print false')
+// }
+
+// var rain=true
+
+// if(rain)
+// {
+//     console.log('take an umberalla')
+// }else{
+//     console.log('enjoy the sunshine')
+// }
+
+// var homework= false
+
+// if(homework){
+//     console.log('you did a great job')
+// }else{
+//     console.log('you have to consentrate')
+// }
+
+// var cookies = false
+
+// if(cookies){
+//     console.log('would you like some cookie')
+// }else{
+//     console.log('time to bake more cookie')
+// }
+
+// var teeth = false
+// if(teeth){
+//     console.log('your teeth are clean')
+// }else{
+//     console.log('go brush your teeth')
+// }
+
+// var bday = false
+
+// if(bday){
+//     console.log('happy bday enjoy your day')
+// }else{
+//     console.log('have a great day')
+// }
+
+// console.log(true && true)
+
+// console.log(true && false)
+
+// console.log(true || false)
+// console.log(true||true)
+
+// console.log(true && false && true)
+// console.log(true || false || true)
+
+
+// var color = 'red'
+
+// if(color == 'red'){
+//     console.log('please stop')
+// }
+
+// if(color == 'yellow'){
+//     console.log('get ready')
+// }
+
+// if(color == 'green'){
+//     console.log('get go')
+// }
+
+// var season = 'winter'
+
+// if(season == 'spring'){
+//     console.log('Enjoy the blossom')
+// }
+
+// if(season == 'summer'){
+//     console.log('have fun in the sun')
+// }
+
+// if(season == 'autumn'){
+//     console.log('admire the colorful leaves')
+// }
+
+// if(season == 'winter'){
+//     console.log('bundle up and stay warm')
+// }
+
+
+// var score = 95
+
+// if(score <=50){
+//     console.log('you have to improve')
+// }
+
+// else if(score >50 && score <=70){
+//     console.log('you have to work hard')
+// }
+
+// else if(score >70){
+//     console.log('you have done a great job')
+// }
+
+
+// var num = 50
+
+// if(num/2==0){
+//     console.log('the given no is even')
+// }
+// else{
+//     console.log('the given no is odd')
+// }
+
+                 /// need to check  ///
+/*
+var text = 'hello'
+
+if(text === 'a','e','i','o','u'){
+    console.log('the given letter is a vowel')
+}else{
+    console.log('the given letter is not an vowel')
+}*/
+
+// for(count=1;count<=10;count=count+1){
+//     console.log('Nawin')
+// }
+
+// var par = document.getElementById('word')
+// console.log(par.textContent)
+// par.textContent='bye'
+
+// var tit = document.getElementById('hi')
+// console.log(hi.textContent)
+// hi.textContent = "bye world"
+
+// var tit = document.getElementById('hi')
+// console.log(hi.textContent)
+
+// function change(){
+//     h1.textContent='bye world'
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
