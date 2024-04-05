4029b73d5f1fe7b34380e8fb3b23033600681e63
Nawin Chander S <Nawinnapzy289@gmail.com>
2024-04-05 19:14:59 +0530

    Add files via upload

---
+++ loops/loops.js
@@ -0,0 +1,23 @@
+// loops
+// for( let i = 1;i<=5;i++ ){
+//     console.log('hello')
+     
+// }
+
+// for( let i = 1;i<=5;i++ ){
+//     console.log(i)  
+// }
+// console.log('outside loop ', i)
+
+// print 10 to 1
+
+for(i=10;i>=1;i--)
+    console.log(i)
+
+j=10
+while(j>=1){
+    console.log(j)
+}
+
+
+
